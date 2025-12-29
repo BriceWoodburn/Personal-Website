ae89760ffcd0572536b6a4875f18df58cf7bb01c
Brice Woodburn <bjw6356@rit.edu>
2025-12-29 18:37:47 -0500

    finished media queries for the main page and added a hamburger menu.
    Made media queries for 700px and below. When the navbar got too small it converts into a hamburger menu.

--- js/main.js
+++ js/main.js
@@ -25,3 +25,23 @@ const observer = new IntersectionObserver(
 );
 
 faders.forEach(el => observer.observe(el));
+
+
+// Hamburger menu
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.querySelector("nav.nav-links");
+const links = document.querySelectorAll("nav.nav-links a");
+
+// Toggle menu on hamburger click
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
+
+// Collapse menu when a link is clicked
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("active");
+  });
+});
